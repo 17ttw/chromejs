692991e5b4ed704451fa77d636734960ea28b5b9
51ttw <329966505@qq.com>
2019-06-06 09:34:13 +0800

    Update chrome_common.js

--- chrome_common.js
+++ chrome_common.js
@@ -308,8 +308,8 @@ function loginmyaccount() {
     var l = location.href;
     var r1 = "http://www.17ttw.com/image/index.html";
     if (r == r1 && l.indexOf("index.html#/login") > 0) {
-        console.log("referrer : %S",r);
-        console.log("href : %S",l);
+        console.log("referrer : %s",r);
+        console.log("href : %s",l);
         clickJs(".ivu-form-item-content>button");
     }
 }
